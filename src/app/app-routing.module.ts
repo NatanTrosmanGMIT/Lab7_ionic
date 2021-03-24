31b508cc1cbb0433ec00b082387eb28f2027787d
Natan Trosman <you@example.comG00347770@gmit.ie>
2021-03-24 15:39:54 +0000

    lab complete

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'ulster',
+    loadChildren: () => import('./ulster/ulster.module').then( m => m.UlsterPageModule)
+  },
+  {
+    path: 'munster',
+    loadChildren: () => import('./munster/munster.module').then( m => m.MunsterPageModule)
+  },
+  {
+    path: 'connaught',
+    loadChildren: () => import('./connaught/connaught.module').then( m => m.ConnaughtPageModule)
+  },
+  {
+    path: 'leinster',
+    loadChildren: () => import('./leinster/leinster.module').then( m => m.LeinsterPageModule)
+  },
+  {
+    path: 'connaught-counties',
+    loadChildren: () => import('./connaught-counties/connaught-counties.module').then( m => m.ConnaughtCountiesPageModule)
+  },
+  {
+    path: 'leinster-counties',
+    loadChildren: () => import('./leinster-counties/leinster-counties.module').then( m => m.LeinsterCountiesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
